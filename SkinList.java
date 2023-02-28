6e3027b2184f5783c2849cfe1596dd888c070486
jcgaming-official <77482435+jcgaming-official@users.noreply.github.com>
2023-02-28 22:27:08 +0800

    Updated SkinList.java
    Updated to the Patch 13.4

--- SkinList.java
+++ SkinList.java
@@ -1,9 +1,8 @@
-﻿import java.io.BufferedReader;
+import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Scanner;
-
 
 public class SkinList {
     public static void main(String[] args) throws Exception {
@@ -12,29 +11,29 @@ public class SkinList {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the champion name:");
         String championName = sc.nextLine();
-        String apiKey = "RGAPI-e5c5867e-b45f-4307-85bc-fe7759cbd643"; // use your own api by replacing this apikey
 
-        String url = "https://ddragon.leagueoflegends.com/cdn/10.19.1/data/en_US/champion/" + championName + ".json";
+        String apiKey = "RGAPI-e5c5867e-b45f-4307-85bc-me49nfla03ja";
+        String url = "https://ddragon.leagueoflegends.com/cdn/13.4.1/data/en_US/champion/" + championName + ".json?api_key=" + apiKey;
 
         URL obj = new URL(url);
         HttpURLConnection con = (HttpURLConnection) obj.openConnection();
 
         con.setRequestMethod("GET");
 
-        BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-        String inputLine;
-        StringBuffer response = new StringBuffer();
+        try (BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()))) {
+            String inputLine;
+            StringBuffer response = new StringBuffer();
 
-        while ((inputLine = in.readLine()) != null) {
-            response.append(inputLine);
-        }
-        in.close();
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            String[] skins = response.toString().split("\"skins\":\\[")[1].split("\\},\\{");
 
-        String[] skins = response.toString().split("\"skins\":")[1].split("\"name\":");
-
-        System.out.println("Skin List for Champion: " + championName);
-        for (int i = 1; i < skins.length; i++) {
-            System.out.println((i) + ". " + skins[i].split("\"")[1]);
+            System.out.println("Skin List for Champion: " + championName);
+            for (int i = 0; i < skins.length; i++) {
+                String skinName = skins[i].split("\"name\":\"")[1].split("\"")[0];
+                System.out.println((i + 1) + ". " + skinName);
+            }
         }
     }
 }
